192871aa5f1be4d14139d462025805b928df55f6
John Kubach <johnkubach@gmail.com>
2018-12-10 19:57:03 -0500

    Add generic prohibited landing page

---
+++ website/res/nope.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<h3> Abandon All Hope, Ye Who Enter Here </h3>";
+    echo "<p> You are not allowed to view this page</p>";
+    echo "<a href='../index.php'> Home </a> <br>";
+    exit();
+?>
