4cd61e072b2ea53e78f57a6fa20289b299212965
John Kubach <johnkubach@gmail.com>
2018-12-10 20:00:19 -0500

    Add addition data to profile page

--- website/profile.php
+++ website/profile.php
@@ -7,6 +7,7 @@ $user = $_SESSION['username'];
 $email = $_SESSION['email'];
 $uid = $_SESSION['uid'];
 $date = $_SESSION['date'];
+$block = $_SESSION['blocked'];
 
 $sql = "SELECT title FROM survey WHERE survey_id IN (SELECT survey_id FROM answer_numeric WHERE user_id=$uid);";
         $result = mysqli_query($connect, $sql);
@@ -18,3 +19,4 @@ $sql = "SELECT title FROM survey WHERE survey_id IN (SELECT survey_id FROM answe
     <h1> <?php echo $email; ?> </h1>
     <h1> <?php echo $uid; ?> </h1>
     <h1> <?php echo $date; ?> </h1>
+    <h1> <?php echo $block; ?> </h1>
