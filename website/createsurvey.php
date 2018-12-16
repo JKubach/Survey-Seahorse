5e29c2f36102472e04e86ac913c83ea7478f8699
jkubach <johnkubach@gmail.com>
2018-12-16 17:00:35 -0500

    update create survey page
    added descriptions as to what each page item does

--- website/createsurvey.php
+++ website/createsurvey.php
@@ -1,16 +1,24 @@
 <html>
-<h2> Create Survey </h2>
+<h2> Create A New Survey </h2>
 <form class="create-survey-form" action="res/new-survey.php" method="POST">
 
+<p> Title & Description </p> 
 <input type="text" name="title" placeholder="My Amazing Survey"> <br>
-<input type="text" name="description" placeholder="A great survey"> <br>
-<input type="radio" name="type" value="00"> Numeric (1 - 10) <br>
+<input type="text" name="description" placeholder="This is a great survey"> <br>
+
+<p> Answer Type </p>
+ <input type="radio" name="type" value="00"> Numeric (1 - 10) <br>
 <input type="radio" name="type" value="01"> Yes / No <br>
 <input type="radio" name="type" value="10"> Text <br>
+
+<p> Number of Questions </p>
 <input type="text" name="number" placeholder="5"> <br>
-<input type="date" name="expire">
-<input type="radio" name="once" value="1"> one shot <br>
-<input type="radio" name="once" value="0" checked> multi
+
+<p> Expiration Date </p>
+<input type="date" name="expire"> <br>
+ <p> Can a user take this survey more than once? </p>
+<input type="radio" name="once" value="0" checked> Yes <br>
+<input type="radio" name="once" value="1"> No <br>
 
 <button type="submit" name="submit">Create Survey</button>
 
