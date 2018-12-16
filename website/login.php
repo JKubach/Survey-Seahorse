25ae8dc87d53052b99240c1b4fe75425000ebe18
David Liotta <liottad6@students.rowan.edu>
2018-12-16 18:33:49 -0500

    Minor formatting change

--- website/login.php
+++ website/login.php
@@ -11,7 +11,7 @@ if (!empty($_GET)) {
 <input type="text" name="username" placeholder="User Name">
 <input type="text" name="password" placeholder="Password">
 <button type="submit" name="submit">Sign In</button>
-    
+<br>
 <a href = "res/nope.php" target = "_blank">Forgot your password?</a> 
 <br>
 <a href = "signUp.php" target = "_blank">Don't have an account?</a>
