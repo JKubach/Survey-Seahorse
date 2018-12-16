8d1029f21668feb95a70dbb3fb13f1f4e72832a8
David Liotta <liottad6@students.rowan.edu>
2018-12-16 17:20:13 -0500

    included navbar

--- website/controlpanel.php
+++ website/controlpanel.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION['admin'])) {
     exit();
 } else {
     $user = $_SESSION['username'];
+    include 'res/navbar.php';
 
     echo "<p> Hello, ", $user, " what would you like to do? <br>";
 }
