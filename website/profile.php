a5f52ee62d5749d213073f8853518c82fdc42149
David Liotta <liottad6@students.rowan.edu>
2018-12-16 17:31:57 -0500

    added navbar

--- website/profile.php
+++ website/profile.php
@@ -18,20 +18,15 @@ if($block == 49)
 if($block == 50)
         $blockStatus = "you shouldn't even be here";
 
+include 'res/navbar.php';
+
 $sql = "SELECT title FROM survey WHERE survey_id IN (SELECT survey_id FROM answer_numeric WHERE user_id=$uid);";
         $result = mysqli_query($connect, $sql);
         $surveys_taken = mysqli_fetch_assoc($result);
         echo  "Surveys that ", $user, " has taken: ", $surveys_taken['title'], "<br>";
 ?>
 
-<link rel = "stylesheet" href= "res/nav.css">
-<div class= nav>
-    <a href="index.php" target="_blank"> Home </a>
-    <a href="profile.php" target="_blank"> <?php echo $user; ?>'s Profile </a>
-    <a href="directory.php" target="_blank"> Surveys </a>
-    <a href="createsurvey.php" target="_blank"> Create a Survey </a>
-</div>
-<br><br>
+
 
     <h2> Username: <?php echo $user; ?> </h2>
     <h2> Email: <?php echo $email; ?> </h2>
