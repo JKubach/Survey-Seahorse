0ee62e8bccde46e4e70b8817aa7be30e438c7809
John Kubach <johnkubach@gmail.com>
2018-12-17 15:09:45 -0500

    addiotion admin message logging

--- website/res/admin-message.php
+++ website/res/admin-message.php
@@ -1,11 +1,14 @@
 <?php
+session_start();
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
 if (!empty($_POST['message'])) {
+    $uid = $_SESSION['uid'];
+    $username = $_SESSION['username'];
     $message = $_POST['message'];
     $var_str = var_export($message, true);
-    $var = "<?php\n\n\$message = $var_str;\n\n?>";
+    $var = "<?php\n\n\$message = $var_str;\n\n\$uid = $uid;\n\n\$user = '$username';\n\n?>";
     file_put_contents("txt/msg.php",$var);
     header("Location: ../controlpanel.php?msg=success");
 } elseif (isset($_POST['remove'])) {
