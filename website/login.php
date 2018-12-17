6811a8294428b752bf01be5794208c2390eb1bae
Andrew <andreww107@gmail.com>
2018-12-16 20:43:52 -0500

    Update login.php
    Rearranged elements for CSS

--- website/login.php
+++ website/login.php
@@ -5,16 +5,15 @@ if (!empty($_GET)) {
 }
 ?>
 <html>
-<h2> Sign In</h2>
+<link rel="stylesheet" href="res/style.css">
+
+
 <form class="signin-form" action="res/login-user.php" method="POST">
-
-<input type="text" name="username" placeholder="User Name">
-<input type="text" name="password" placeholder="Password">
-<button type="submit" name="submit">Sign In</button>
-<br>
-<a href = "resestPass.php" target = "_blank">Forgot your password?</a> 
-<br>
-<a href = "signUp.php" target = "_blank">Don't have an account?</a>
-
+    <h2> Sign In</h2>
+    <input type="text" name="username" placeholder="User Name">
+    <input type="text" name="password" placeholder="Password">
+    <button type="submit" name="submit">Sign In</button> <br>
+    <a href = "resestPass.php" target = "_blank">Forgot your password?</a> <br>
+    <a href = "signUp.php" target = "_blank">Don't have an account?</a>
 </form>
 </html>
