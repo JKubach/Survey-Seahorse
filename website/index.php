0299aa86f6292fb6774afb8f2c309fb52e5f8f27
Andrew <andreww107@gmail.com>
2018-12-16 19:50:55 -0500

    Update index.php
    Rearranged elements and added divs for CSS

--- website/index.php
+++ website/index.php
@@ -21,20 +21,27 @@ $newUser = mysqli_fetch_assoc($result);
 <link rel="mask-icon" href="res/img/fav/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
+<link rel="stylesheet" href="res/style.css">
 </head>
 
 <?php include 'res/navbar.php';?>
 
-<h1> Survey Seahorse </h1>
-<h3> Random Question: <?php echo $q['question_content']; ?> </h3>
-<a href="login.php"> Login </a> <br>
-<a href="signup.php"> Register </a> <br>
-<a href="profile.php"> Profile </a> <br>
-<a href="createsurvey.php"> Create Survey </a> <br>
-<a href="findsurvey.php"> Find a Survey </a> <br>
-<a href="directory.php"> Survey Directory</a> <br>
-<a href="logout.php"> Logout </a> <br>
+<div class="panel">
+    <h1> Survey Seahorse </h1>
+    <h3> Random Question: <?php echo $q['question_content']; ?>
+    <img src="res/img/seahorse.png" class="seahorse_pic" size="250">
+    <h3> Welcome to our newest user: <?php echo $newUser['username']; ?> </h3>
+</div>
 
+<div class="index-grid-panel">
+    <a href="login.php"> Login </a>
+    <a href="signup.php"> Register </a>
+    <!--<a href="profile.php"> Profile </a> <br>
+    <a href="createsurvey.php"> Create Survey </a> <br>-->
+    <a href="findsurvey.php"> Find a Survey </a>
+    <a href="directory.php"> Survey Directory</a>
+    <a class="off" href="logout.php"> Logout </a>
+</div>
 
 <?php 
 if (isset($_SESSION['uid'])) {
@@ -53,5 +60,5 @@ if (isset($_SESSION['uid'])) {
 }
 ?>
 
-<p> Welcome to our newest user: <?php echo $newUser['username']; ?> </p>
+
 
