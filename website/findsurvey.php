879d4ff11eaf7cf651f1d3718c9b17c6d61536bc
Andrew <andreww107@gmail.com>
2018-12-16 21:00:29 -0500

    Update findsurvey.php

--- website/findsurvey.php
+++ website/findsurvey.php
@@ -5,10 +5,12 @@ $sql = "SELECT access_code FROM survey ORDER BY RAND() LIMIT 1;";
 $result = mysqli_query($connect, $sql);
 $rand_code = mysqli_fetch_assoc($result);
 ?>
-<h2> Find Survey </h2>
+<link rel="stylesheet" href="res/style.css">
+
 <form class ="find-survey-form" action="takesurvey.php" method="POST">
-<input type="text" name="survey-code" placeholder="Survey Code">
-<button type="submit" name="submit">Find Survey</button>
+  <h2> Find Survey </h2>
+  <input type="text" name="survey-code" placeholder="Survey Code">
+  <button type="submit" name="submit">Find Survey</button>
 <?php 
 echo "<p> Looking for a survey? Try ", $rand_code['access_code'], "</p>"; 
 
