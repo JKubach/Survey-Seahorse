7de99ce7b155d792aa4a5bd604ae05c95934f6c4
Andrew <andreww107@gmail.com>
2018-12-16 20:53:33 -0500

    Update signup.php
    CSS edits

--- website/signup.php
+++ website/signup.php
@@ -1,11 +1,9 @@
 <html>
-<h2> Signup </h2>
 <form class="signup-form" action="res/register.php" method="POST">
-
-<input type="text" name="email" placeholder="Email">
-<input type="text" name="username" placeholder="User Name">
-<input type="password" name="password" placeholder="Password">
-<button type="submit" name="submit">Sign Up</button>
-
+  <h2> Signup </h2>
+  <input type="text" name="email" placeholder="Email">
+  <input type="text" name="username" placeholder="User Name">
+  <input type="password" name="password" placeholder="Password">
+  <button type="submit" name="submit">Sign Up</button>
 </form>
 </html>
