e1626698b54e19efb70ab2866307e9453236760d
John Kubach <johnkubach@gmail.com>
2018-12-17 22:10:09 -0500

    placeholder text for text boxes

--- website/questions.php
+++ website/questions.php
@@ -8,16 +8,16 @@ echo '<h2> Enter the questions for your survey </h2>';
 
 if (isset($_SESSION['questions'])) {
     $questions = $_SESSION['questions'];
-    $questionNum = $counter + 1;
     echo "<form class='question-form' method = 'POST' action = 'res/submit-questions.php'>";
     for($counter = 0; $counter < $questions; $counter++) {
+        $questionNum = $counter + 1;
         echo "<input type = 'text' name = 'questions[]' class = 'questions' placeholder='Question $questionNum' /><br/>";
     }
     echo "<input type = 'submit' value = 'SEND'/>";
     echo "</form>";
 } else {
-        header("Location: res/nope.php");
-        exit();
+    header("Location: res/nope.php");
+    exit();
 }
 ?>
 </html>
