9f91b7d63bb0f67d0241e803804ace9b04d8e772
John Kubach <johnkubach@gmail.com>
2018-12-17 22:11:41 -0500

    error check for blank questions
    questions that are left blank are no longer accepted. any instance of a question that is not filled out will kick a user back to the questions page.

--- website/res/submit-questions.php
+++ website/res/submit-questions.php
@@ -5,21 +5,30 @@ ini_set('session.cache_limiter','public');
 session_cache_limiter(false);
 session_start();
 include_once '../config.php';
-	$questions = array();
-	
-    $survey_id = $_SESSION['sid'];
-	$questions = $_POST['questions'];
-	
-	for($counter = 0; $counter < sizeof($questions); $counter++)
-	{
-        $q = $questions[$counter];
-		echo "Question #".($counter + 1).": ".$questions[$counter]."<br />";
-        $sql = "INSERT INTO question (survey_id, question_number, question_content)
-                VALUES ('$survey_id', $counter + 1, '$q');";
-        mysqli_query($connect, $sql) or die(mysqli_error($connect));
+$questions = array();
 
-    }
-        unset($_SESSION["questions"]);
-        header("Location: ../success.php");
+$survey_id = $_SESSION['sid'];
+$questions = $_POST['questions'];
+
+for($counter = 0; $counter < sizeof($questions); $counter++)
+{
+    $q = $questions[$counter];
+
+    if (empty($q)) {
+        header("Location: ../questions.php?err=notcomplete");
         exit();
+    }
+}
+
+for($counter = 0; $counter < sizeof($questions); $counter++){
+    echo "Question #".($counter + 1).": ".$questions[$counter]."<br />";
+    $sql = "INSERT INTO question (survey_id, question_number, question_content)
+        VALUES ('$survey_id', $counter + 1, '$q');";
+    mysqli_query($connect, $sql) or die(mysqli_error($connect));
+}
+
+
+unset($_SESSION["questions"]);
+header("Location: ../success.php");
+exit();
 ?>
