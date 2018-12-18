04239e406be418888527bb8ce7938b7c4120c49e
John Kubach <johnkubach@gmail.com>
2018-12-17 22:42:46 -0500

    change survey deletion
    change order in which survey data is removed from the database.

--- website/res/delete-survey.php
+++ website/res/delete-survey.php
@@ -18,13 +18,13 @@ if (isset($_POST['delete-survey'])) {
         $result = mysqli_query($connect, $sql);
         $row = mysqli_fetch_assoc($result);
         $sid = $row['survey_id'];
-        $deleteSurvey ="DELETE FROM survey WHERE survey_id=$sid;";
-        $deleteQuestions ="DELETE FROM question WHERE survey_id=$sid;";
         $deleteAnswers ="DELETE FROM answer_numeric WHERE survey_id=$sid;";
+        $deleteQuestions ="DELETE FROM question WHERE survey_id=$sid;";
+        $deleteSurvey ="DELETE FROM survey WHERE survey_id=$sid;";
 
-        mysqli_query($connect, $deleteSurvey) or die(mysqli_error($connect));
-        mysqli_query($connect, $deleteQuestions) or die(mysqli_error($connect));
         mysqli_query($connect, $deleteAnswers) or die(mysqli_error($connect));
+        mysqli_query($connect, $deleteQuestions) or die(mysqli_error($connect));
+        mysqli_query($connect, $deleteSurvey) or die(mysqli_error($connect));
         exit();
     }
 }
