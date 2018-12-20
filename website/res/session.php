d3eb338f5adaeade104743f47d4a8a62d91d179f
John Kubach <johnkubach@gmail.com>
2018-12-20 18:15:14 -0500

    css changes

--- website/res/session.php
+++ website/res/session.php
@@ -5,3 +5,12 @@ ini_set('session.cache_limiter','public');
 session_cache_limiter(false);
 session_start();
 ?>
+
+<link rel="apple-touch-icon" sizes="180x180" href="res/img/fav/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="res/img/fav/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="res/img/fav/favicon-16x16.png">
+<link rel="manifest" href="res/img/fav/site.webmanifest">
+<link rel="mask-icon" href="res/img/fav/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+<link rel="stylesheet" href="res/style.css">
