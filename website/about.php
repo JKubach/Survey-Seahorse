3835ea36de0f9b7b6447aa5112a806bf3feb8835
Andrew <andreww107@gmail.com>
2018-12-20 17:57:44 -0500

    Update about.php

--- website/about.php
+++ website/about.php
@@ -16,10 +16,10 @@ include 'res/navbar.php';
         <h3> Developers: Jack Donahue, Alamine Elasmai, David Liotta, Andrew Weatherby</h3>
         <br>
         <h1> Credits </h1>
-        <h3> <a href="http://phpqrcode.sourceforge.net/">QR Code Generation</a>
-        <h3> <a href="https://www.chartjs.org/">Chart Generation</a> </h3>
+        <h3> <a href="http://phpqrcode.sourceforge.net/" class='link'>QR Code Generation</a>
+        <h3> <a href="https://www.chartjs.org/" class='link'>Chart Generation</a> </h3>
         <br>
-        <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div>Icons made by <a class='link' href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" class='link' 			    title="Flaticon">www.flaticon.com</a> licensed by <a  class='link' href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     
 
     </body>
